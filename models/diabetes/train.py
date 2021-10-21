fe8451846db7733e53b350540162d51d1813ef7c
Nicolas CLERC <nicolas.clerc@sablefin.net>
2021-10-21 11:31:29 +0200

    update model

--- models/diabetes/train.py
+++ models/diabetes/train.py
@@ -44,4 +44,4 @@ for alpha in alphas:
     with open(os.path.join('./outputs/', model_file_name), 'wb') as file:
         pickle.dump(reg, file)
 
-    print('alpha is {0:.2f}, and mse is {1:0.2f}'.format(alpha, mse))
+    print('Resultat du training: alpha is {0:.2f}, and mse is {1:0.2f}'.format(alpha, mse))
